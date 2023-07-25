2fc36d138b38fe730fdb0f56b707f4069a0d0a77
Elisa Edson <e.edson@protonmail.ch>
2023-07-25 14:29:18 +0100

    idk

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import axios from "axios";
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  console.log(process.env.REAC_APP_KEY);
+  
+  const [SearchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+  const [map, setMap] = useState("");
+
+  async function getLocation(event){
+    //event.target.input.value = "";
+    event.preventDefault();
+    const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_KEY}N&q=${SearchQuery}&format=json`;
+    let result = await axios.get(API);
+    setLocation(result[0]);
+    console.log(API);
+  }
+
+  function handleSearch(event){
+    setSearchQuery(event.target.value);
+  }
+
   return (
     <div className="App">
-      <form>
-      <input type="text" placeholder="Explore!" name="input"/>
+      <form onSubmit={getLocation}>
+      <input type="text" placeholder="Find a Destination" name="input" onChange={handleSearch}/>
       <button type="submit">Explore!</button>
       </form>
+
+      <p>{location.display_name}</p>
     </div>
   );
 }
