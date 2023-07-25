95c73cebc865683bb584008e3ae20f383b0ad192
Elisa Edson <e.edson@protonmail.ch>
2023-07-25 17:04:14 +0100

    working explore map

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ function App() {
   let REACT_APP_KEY = "pk.0988406d58ca554ca26fe1365768841f";
   const [SearchQuery, setSearchQuery] = useState("");
   const [location, setLocation] = useState({});
+  const [map, setMap] = useState("");
 
   async function getLocation(event){
     //event.target.input.value = "";
     event.preventDefault();
     const API = `https://eu1.locationiq.com/v1/search?key=${REACT_APP_KEY}&q=${SearchQuery}&format=json`;
     let result = await axios.get(API);
-    setLocation(result[0]);
+    console.log(result);
+    setLocation(result.data[0]);
+    handleMap(result);
     console.log(API);
   }
 
@@ -21,6 +24,10 @@ function App() {
     setSearchQuery(event.target.value);
   }
 
+  function handleMap(result){
+    const API = `https://maps.locationiq.com/v3/staticmap?key=${REACT_APP_KEY}&center=${result.data[0].lat},${result.data[0].lon}&zoom=9`;
+    setMap(API);
+  }
   return (
     <div className="App">
       <form onSubmit={getLocation}>
@@ -29,6 +36,7 @@ function App() {
       </form>
 
       <p>{location.display_name}</p>
+      <p>{map && <img src={map} alt="map" />}</p>
     </div>
   );
 }
